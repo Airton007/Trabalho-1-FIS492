59f66943a0ffa073411e97a667782eb6e4decc36
Airton007 <airtondfranca@gmail.com>
2024-11-13 23:26:54 -0300

    Declara variáveis e Le arquivo

--- program_estatistica.f90
+++ program_estatistica.f90
@@ -1,5 +1,32 @@
 program estatistica
         use, intrinsic :: iso_fortran_env, only: sp=>real32, dp=>real64, i4=>int32
     implicit none
+    !!!!!!!!!!Declaração das variáveis!!!!!!!!!!! 
+    !Variáveis auxiliares
+    integer(kind=i4) :: arquivo, i
+    integer(kind=i4) :: N
+
+    !Pares de pontos e medias
+    real(kind=dp), allocatable :: x(:),y(:)
     
+    !Pares de pontos e medias
+    real(kind=dp) :: media_x, media_y, soma_x, soma_y
+    
+    !Vairancias, desvios padrão e coeficiente de correlação
+    real(kind=dp) :: S_x, S_y, desvio_x, desvio_y, r_xy
+    
+    open(newunit=arquivo, file="dados.dat", action="read")
+
+    !Conta o número de linhas do arquivo
+    N = 0
+    do
+        read(arquivo, *, iostat=i)
+        if ( i/=0 ) exit
+            N = N + 1
+    end do
+    close(arquivo)
+
+    !Aloca nos pares de varáveis a quantidades de ponto
+    allocate(x(1:N),y(1:N))
+
 end program estatistica
