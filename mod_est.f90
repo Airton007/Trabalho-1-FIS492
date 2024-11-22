e2d4d418a2e30dccb7887e7ec77704a3d248833d
airtondfranca@gmail.com <airton.ferreira@ufv.br>
2024-11-22 01:07:20 -0300

    Adiciona o modo de operação "Manual"

--- mod_est.f90
+++ mod_est.f90
@@ -1,58 +1,50 @@
-module calculo_est
+module est_basic_mod
     use, intrinsic :: iso_fortran_env, only: sp=>real32, dp=>real64, i4=>int32
     implicit none
-    
-    !Somatoria dos termos
-    contains
-     function soma_termos(x,N) result(res)
-        integer(kind=i4) :: i
-        integer(kind=i4), intent(in) :: N
-        real(kind=dp) :: soma, res
-        real(kind=dp) :: x(1:N)
-        soma = 0.0_dp
-        do i = 1, N
-            soma = soma + x(i)
-        end do
-        res = soma
-     end function
 
-    !Cálculo da Soma dos Quadrados dos desvios "SQD_x", onde x pode ser uma variável qualquer
-    function SQD(x,N) result(res)
-        integer(kind=i4) :: i
+contains
+    ! Função para a soma dos termos
+    function soma_termos(x, N) result(res)
         integer(kind=i4), intent(in) :: N
-        real(kind=dp) :: soma, res
-        real(kind=dp) :: x(1:N)
-        soma = 0.0_dp
-        do i = 1, N
-            soma = soma + (x(i) - soma_termos(x,N)/N)**2.0_dp
-        end do
-        res = soma 
-    end function
+        real(kind=dp), intent(in) :: x(1:N)
+        real(kind=dp) :: res
+        res = sum(x)
+    end function soma_termos
 
-    !Cálculo da Soma dos Produtos dos desvios "SPD_x"
-    function SPD(x,N) result(res)
-        integer(kind=i4) :: i
+    ! Função para a média dos termos
+    function media(x, N) result(res)
         integer(kind=i4), intent(in) :: N
-        real(kind=dp) :: soma, res
-        real(kind=dp) :: x(1:N)
-        soma = 0.0_dp
-        do i = 1, N
-            soma = soma + (x(i) - soma_termos(x,N)/N)
-        end do
-        res = soma 
-    end function
+        real(kind=dp), intent(in) :: x(1:N)
+        real(kind=dp) :: res
+        res = sum(x) / N*1.0_dp
+    end function media
 
-    !Cálculo da Soma dos Produtos dos desvios "SPD_xy", de x e y
-    function SPD_xy(x,y,N) result(res)
-        integer(kind=i4) :: i
+    ! Função para o cálculo da soma dos quadrados dos desvios (SQD)
+    function SQD(x, N) result(res)
         integer(kind=i4), intent(in) :: N
-        real(kind=dp) :: soma, res
-        real(kind=dp) :: x(1:N), y(1:N)
-        soma = 0.0_dp
-        do i = 1, N
-            soma = soma + ((x(i) - soma_termos(x,N)/N)*(y(i) - soma_termos(y,N)/N))
-        end do
-        res = soma 
-    end function
+        real(kind=dp), intent(in) :: x(1:N)
+        real(kind=dp) :: res, media_x
+        media_x = media(x, N)
+        res = sum((x - media_x)**2.0_dp)
+    end function SQD
 
-end module calculo_est
+    ! Função para a soma dos produtos dos desvios (SPD_x)
+    function SPD(x, N) result(res)
+        integer(kind=i4), intent(in) :: N
+        real(kind=dp), intent(in) :: x(1:N)
+        real(kind=dp) :: res, media_x
+        media_x = media(x, N)
+        res = sum((x - media_x)**2.0_dp)
+    end function SPD
+
+    ! Função para a soma dos produtos dos desvios entre dois vetores (SPD_xy)
+    function SPD_xy(x, y, N) result(res)
+        integer(kind=i4), intent(in) :: N
+        real(kind=dp), intent(in) :: x(1:N), y(1:N)
+        real(kind=dp) :: res, media_x, media_y
+        media_x = media(x, N)
+        media_y = media(y, N)
+        res = sum((x - media_x) * (y - media_y))
+    end function SPD_xy
+
+end module est_basic_mod
--- program_estatistica.f90
+++ program_estatistica.f90
@@ -1,67 +1,98 @@
 program estatistica
-    use calculo_est
+    use est_basic_mod
+    use rndgen_mod
         use, intrinsic :: iso_fortran_env, only: sp=>real32, dp=>real64, i4=>int32
     implicit none
+
     !!!!!!!!!!Declaração das variáveis!!!!!!!!!!! 
     !Variáveis auxiliares
     integer(kind=i4) :: arquivo, i
     integer(kind=i4) :: N
-
+    character(len=100) :: escolha
     !Pares de pontos e medias
     real(kind=dp), allocatable :: x(:),y(:)
-    
+
     !Pares de pontos e medias
     real(kind=dp) :: media_x, media_y, soma_x, soma_y
     
     !Vairancias, desvios padrão e coeficiente de correlação
     real(kind=dp) :: S_x, S_y, desvio_x, desvio_y, r_xy
     
-    open(newunit=arquivo, file="dados.dat", action="read")
+    write(*,*)"Digite ""S"" se deseja escrever os valores manualmente ou ""N"" &
+                se deseja usar um arquido do tipo .dat "
+    read(*,*) escolha
 
-    !Conta o número de linhas do arquivo
-    N = 0
-    do
-        read(arquivo, *, iostat=i)
-        if ( i/=0 ) exit
-            N = N + 1
-    end do
-    close(arquivo)
+    if (escolha == "S" .or. escolha == "s") then
+   
+        Write(*,*) "Escreva o número de termos ""N"" "
+        read(*,*) N
 
-    !Aloca nos pares de varáveis a quantidades de ponto
-    allocate(x(1:N),y(1:N))
+        allocate(x(N), y(N))
 
-    open(newunit=arquivo, file="dados.dat", action="read")
+        write(*, *) "Escreva os pares de valores x e y(x)"
     
-    soma_x = 0.0_dp
-    soma_y = 0.0_dp
-    !Le os pontos x e y do arquivo dados.dat
-    do i = 1, N
-       read(arquivo, *) x(i), y(i)
-    end do
-    close(arquivo)
+        open(newunit=arquivo, file="dados_testes.dat", action="write", status="replace")
 
-    media_x = soma_termos(x,N)/N   
-    media_y = soma_termos(y,N)/N
-    S_x = SQD(x,N)/N-1
-    S_y = SQD(y,N)/N-1
-    desvio_x = (S_x)**0.5_dp
-    desvio_y = (S_y)**0.5_dp
+        do i=1, N
+            write(*,'(A,I0,A)') "Digite o valor de x(",i, "): "
+            read(*,*) x(i)
 
-    !Calculo do coeficiente de correlação (Pearson)
-    r_xy = SPD_xy(x,y,N)/(SQD(x,N)*SQD(y,N))**0.5_dp
+            write(*,'(A,I0,A)') "Digite o valor de y(",i, "): "
+            read(*,*) y(i)
 
-    !if ( r_xy ) then
+            ! Escrever os pares no arquivo
+            write(arquivo,*) x(i), y(i)
+        end do
+        close(arquivo)
+
+    else if (escolha == "N" .or. escolha == "n") then
+
+            !Conta o número de linhas do arquivo
+            N = 0
+            do
+                read(arquivo, *, iostat=i) !"iostat" retorna o status da leitura dos dados. Caso exista i = 0 caso não i/=0
+                   if ( i/=0 ) exit
+                   N = N + 1
+            end do
+            close(arquivo)
+            
+            !Aloca nos pares de varáveis a quantidades de ponto
+            allocate(x(1:N),y(1:N))    
         
-    !end if
-   !!!!!!!!!Programa principal!!!!!!!!!!!!!
+            soma_x = 0.0_dp
+            soma_y = 0.0_dp
+        
+            open(newunit=arquivo, file="dados.dat", action="read")
+            
+            !Le os pontos x e y do arquivo dados.dat
+            do i = 1, N
+               read(arquivo, *) x(i), y(i)
+            end do
+            close(arquivo)
+    end if
 
-print*, "A media de x é:", media_x
-print*, "A media de y é:", media_y
-print*, "A variância de x é:", S_x
-print*, "A variância de y é:", S_y
-print*, "O desvio padrão de x é:", desvio_x
-print*, "O desvio padrão de y é:", desvio_y
-print*, "O coeficiente de Pearson é:", r_xy
+    !Cálculo das médias
+    media_x = media(x,N)  
+    media_y = media(y,N)
+    print*, "A media de x é:", media_x
+    print*, "A media de y é:", media_y
 
+    !Cálculo das variâncias
+    S_x = SQD(x,N)/(N-1)
+    S_y = SQD(y,N)/(N-1)
+    print*, "A variância de x é:", S_x
+    print*, "A variância de y é:", S_y
+
+    !Cálculo dos desvios
+    desvio_x = sqrt(S_x)
+    desvio_y = sqrt(S_y)
+    print*, "O desvio padrão de x é:", desvio_x
+    print*, "O desvio padrão de y é:", desvio_y
+
+    !Cálculo do coeficiente de correlação (Pearson)
+    r_xy = SPD_xy(x,y,N)/sqrt(SQD(x,N)*SQD(y,N))
+    print*, "O coeficiente de Pearson é:", r_xy
+
+deallocate(x, y)
 
 end program estatistica
